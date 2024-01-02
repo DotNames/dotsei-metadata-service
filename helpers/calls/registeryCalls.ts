908c05997eeeef11a1590abdd6c0184ac66fbc2a
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-01-02 16:15:07 +0530

    chore: added sei suppport

--- helpers/calls/registeryCalls.ts
+++ helpers/calls/registeryCalls.ts
@@ -3,30 +3,60 @@ import { Contract } from "@ethersproject/contracts";
 import { DEFAULT_DOMAIN_EXTENSION } from "../../configs";
 import { getResolverAddress } from "../addressHelpers";
 
-export const getDomainData = async (
-  registryContract: Contract,
-  label: string
-) => {
+const { getCosmWasmClient } = require("@sei-js/core");
+const { getRegistryAddress } = require("@dotnames/dotseijs");
+const { default: DotSei } = require("@dotnames/dotseijs");
+
+export const getDomainData = async (label: string) => {
   try {
     // const namehashLabel = namehash(label);
     const namehashLabel = label;
-    const txData = await registryContract.labelSearch(
-      namehashLabel?.toLowerCase()
-    );
-    const [isRegistered, owner, resolver] = txData;
+    // const txData = await registryContract.labelSearch(
+    //   namehashLabel?.toLowerCase()
+    // );
+    // const [isRegistered, owner, resolver] = txData;
+    // const parsedData = {
+    //   isRegistered,
+    //   owner,
+    //   resolver,
+    //   name: label?.toLowerCase(),
+    // };
+    // if (!isRegistered) {
+    //   throw {
+    //     message:
+    //       "Domain not registered. Please register the domain to continue.",
+    //     code: 404,
+    //   };
+    // }
+    // return parsedData;
+
+    const client = await getCosmWasmClient("https://sei-rpc.polkachu.com/");
+    const dotSei = new DotSei({
+      client,
+      networkId: "pacific-1",
+      dotSeiAddr: getRegistryAddress("pacific-1"), //optional
+    });
+
+    const owner = await dotSei.name(namehashLabel + ".sei").getOwner();
+    const resolver = await dotSei.name(namehashLabel + ".sei").getResolver();
+    const address = await dotSei.name(namehashLabel + ".sei").getAddress();
+    const avatar = await dotSei.name(namehashLabel + ".sei").getAvatar();
+
     const parsedData = {
-      isRegistered,
+      isRegistered: true,
       owner,
       resolver,
       name: label?.toLowerCase(),
     };
-    if (!isRegistered) {
+
+    if (!owner) {
       throw {
         message:
           "Domain not registered. Please register the domain to continue.",
         code: 404,
       };
     }
+
     return parsedData;
   } catch (error) {
     console.log(error);
--- pages/api/metadata/[_domainName].ts
+++ pages/api/metadata/[_domainName].ts
@@ -1,6 +1,6 @@
 import { DEFAULT_CHAIN_ID } from "./../../../configs/index";
 import { getRegistryContract } from "./../../../helpers/contractHelpers";
-import { return_url, parseDomainNameWithShm } from "../../../utils/utils";
+import { return_url, parseDomainNameWithSei } from "../../../utils/utils";
 import { createReferralSvg } from "../../../utils/createSvgReferral";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getDomainData } from "../../../helpers/calls/registeryCalls";
@@ -9,31 +9,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { _domainName = "" } = req.query;
 
   // TODO : Dynamic Chain Id through req query
-  const chainId = DEFAULT_CHAIN_ID;
+  // const chainId = DEFAULT_CHAIN_ID;
   const host = return_url(req);
-  const registeryContract = getRegistryContract(chainId);
-  const parsedDomainNameNoExt = parseDomainNameWithShm(_domainName);
+  // const registeryContract = getRegistryContract(chainId);
+  const parsedDomainNameNoExt = parseDomainNameWithSei(_domainName);
+
+  function domainTrait(domain: string) {
+    switch (domain.length) {
+      case 3:
+        return "Premium";
+      case 4:
+        return "Rare";
+      default:
+        return "Standard";
+    }
+  }
+
   if (!parsedDomainNameNoExt) {
     return res
       .status(404)
-      .json({ message: "Invalid Domain | Label shoud have extension of .shm" });
+      .json({ message: "Invalid Domain | Label shoud have extension of .sei" });
   }
   try {
-    const domainData = await getDomainData(
-      registeryContract,
-      parsedDomainNameNoExt
-    );
+    const domainData = await getDomainData(parsedDomainNameNoExt);
     res.send({
       ...domainData,
       name: parsedDomainNameNoExt,
-      description: `DotShm domains is your go-to web3 identity on the Shardeum blockchain. Replace your complex blockchain addresses using DotShm and get access to thousands of web3 applications, trade your domains on secondary market & much more.
+      description: `Web3 domains natively on Sei Layer 1 blockchain. Currently live on Sei Mainnet. Powered by Sei token.
 
 
-Visit www.dotshm.me now to claim a domain for yourself.`,
+Visit www.dotsei.me now to claim a domain for yourself.`,
       domainName: _domainName,
-      externalUrl: `https://dotshm.me/name/${_domainName}`,
+      externalUrl: `https://dotsei.me/name/${_domainName}`,
       nftUrl: `${host}/api/nft/${_domainName}`,
-      referralUrl: `https://dotshm.me?referral=${parsedDomainNameNoExt}`,
+      referralUrl: `https://dotsei.me?referral=${parsedDomainNameNoExt}`,
       refrralPreviewUrl: `${host}/api/nft/referral/${_domainName}`,
       attributes: [
         {
@@ -50,7 +59,7 @@ Visit www.dotshm.me now to claim a domain for yourself.`,
         },
         {
           trait_type: "type",
-          value: "standard",
+          value: domainTrait(parsedDomainNameNoExt),
         },
       ],
     });
